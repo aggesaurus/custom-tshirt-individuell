0eadc646a1c77e9e26a2288bc1e049db2a6bcb8c
aggesaurus <august.toresson@outlook.com>
2019-10-21 22:22:31 +0200

    cleaned check-all-order_rows-inserted.js

--- rest-queries/16-check-all-order_rows-inserted.js
+++ rest-queries/16-check-all-order_rows-inserted.js
@@ -3,24 +3,7 @@ module.exports = ({ assert, response, store }) => ({
   path: 'order_rows',
   method: 'get',
   test() {
-    // check that we got the right number of users
-    // (intentionelly spelled length wrong to provoke a fail)
     assert.equal(response.length, store.mockOrderRows.length);
-    // check that all users where
-    // inserted correctly with correct data
-    // try{
-    // for (let i = 0; i < response.length; i++){
-    //   let a;
-    //   try {
-    //     a = assert.deepEqual(response[i], store.mockOrderRows[i])
-    //   }
-    //   catch(e){ console.log("NOT EQUAL", e)}
-    //   console.log("response", response[i]);
-    //   console.log("mockOrderRows", store.mockOrderRows[i]);
-    // }
     assert.deepEqual(response, store.mockOrderRows);
-  // }catch(e){
-  //   console.log(e);
-  // }
   }
 });
